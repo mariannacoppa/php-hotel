b2f9533b73e106738ad4beb06e68b645a16e1899
mariannacoppa <densimetra@gmail.com>
2024-09-06 17:29:03 +0200

    [UPD] created select, input and button

---
+++ bonus.php
@@ -0,0 +1,120 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">BoolHotels</h1>
+            </div>        
+        </div>
+        <div class="row mt-4">
+            <div class="col-12">
+                <form action="./bonus.php" method="GET">
+                    <div class="row">
+                        <div class="col-5">
+                            <select name="parking" id="parking" class="form-select form-select-sm">
+                                <option value="">Parcheggio</option>
+                                <option value="0">No</option>
+                                <option value="1">Sì</option>
+                            </select>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" class="form-control form-control-sm" name="vote" placeholder="voto">
+                        </div>
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-sm btn-primary">Cerca</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-12">
+                <h1 class="text-center">PHP Hotel</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Descrizione</th>
+                            <th>Parcheggio</th>
+                            <th>Voto</th>
+                            <th>Distanza dal centro</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($hotels as $hotel) { ?>
+                            <tr>
+                                <td>
+                                    <?php echo $hotel['name']; ?>
+                                </td>
+                                <td>
+                                    <?php 
+                                        echo str_replace('Descrizione', '', $hotel['description']);
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['parking'] ? 'Sì' : 'No'; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['vote']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['distance_to_center']; ?> Km
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
